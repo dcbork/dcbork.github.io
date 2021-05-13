ad0e7f8148c9ac48a222270c37e6dfaf02c5d367
dcbork <cbork13@gmail.com>
2021-05-13 16:32:29 -0400

    basic website

--- js/dynamicPage.js
+++ js/dynamicPage.js
@@ -1,11 +1,20 @@
 var iDo = ["make", "research", "build", "design", "test", "print", "explore"];
 i = 0;
+var date = new Date();
+
 function dynamicSkills() {
    var title = iDo[i];
    $("#dynamicSkills").text(title);
    i = (i+1) % iDo.length;
 }
-dynamicSkills(0);
+dynamicSkills();
 
+function dynamicCopywrite() {
+   var currentYear = date.getFullYear();
+   $("#dynamicCopywrite").text(currentYear);
+}
 
 setInterval(dynamicSkills, 1000);
+window.onload = function() {
+  dynamicCopywrite();
+};
