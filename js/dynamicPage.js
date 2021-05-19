cd2c18d1963b4ceb8046d2ccde990a82271460bd
cbork <cbork13@gmail.com>
2021-05-19 11:14:42 -0400

    updated colors and some of the dynamic page javascript.

--- js/dynamicPage.js
+++ js/dynamicPage.js
@@ -1,19 +1,24 @@
 var iDo = ["make", "research", "build", "design", "test", "print", "study", "explore"];
 var iContact= ["Reach", "Talk to", "Work with", "Ask"]
+var palette = ['#3993DD', '#E63946', '#688E26','#FAA613'];
 iD = 0;
 iC = 0;
 var date = new Date();
 
 function dynamicSkills() {
    var title = iDo[iD];
+   var randomColor = palette[Math.floor(Math.random() * palette.length)];
    $("#dynamicSkills").text(title);
+   $("#dynamicSkills").css('color', randomColor);
    iD = (iD+1) % iDo.length;
 }
 dynamicSkills();
 
 function dynamicContact() {
    var contact = iContact[iC];
+   var randomColor = palette[Math.floor(Math.random() * palette.length)];
    $("#dynamicContact").text(contact);
+   $("#dynamicContact").css('color', randomColor);
    iC = (iC+1) % iContact.length;
 }
 dynamicContact();
@@ -23,8 +28,17 @@ function dynamicCopywrite() {
    $("#dynamicCopywrite").text(currentYear);
 }
 
-setInterval(dynamicSkills, 1013);
+function randomHighlight() {
+   var i;
+   for (i = 0; i <= 9; i++) {
+      var randomColor = palette[Math.floor(Math.random() * palette.length)];
+      $("#pop"+i.toString()).css('color', randomColor);
+   }
+}
+
+setInterval(dynamicSkills, 1319);
 setInterval(dynamicContact, 1913);
 window.onload = function() {
   dynamicCopywrite();
+  randomHighlight();
 };
