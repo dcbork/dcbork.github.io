3c308688a30c9cbd7a4072a5195c30d627da5d46
cbork <cbork13@gmail.com>
2021-06-05 18:54:29 -0400

    updated intactions and style

--- js/dynamicPage.js
+++ js/dynamicPage.js
@@ -1,6 +1,6 @@
 var iDo = ["make", "research", "build", "design", "test", "print", "study", "explore", "do"];
 var iContact= ["Reach", "Talk to", "Work with", "Ask"]
-var palette = ['#3993DD', '#E63946', '#688E26','#FAA613'];
+var palette = ["#3993DD", "#E63946", "#688E26","#FAA613"];
 var tempColor;
 iD = 0;
 iC = 0;
@@ -26,7 +26,7 @@ function dynamicContact() {
    var contact = iContact[iC];
    var randomColor = palette[Math.floor(Math.random() * palette.length)];
    $("#dynamicContact").text(contact);
-   $("#dynamicContact").css('color', randomColor);
+   $("#dynamicContact").css("color", randomColor);
    iC = (iC+1) % iContact.length;
 }
 dynamicContact();
@@ -40,17 +40,82 @@ function randomHighlight() {
    var i;
    for (i = 0; i <= 9; i++) {
       var randomColor = palette[Math.floor(Math.random() * palette.length)];
-      $("#pop"+i.toString()).css('color', randomColor);
+      $("#pop"+i.toString()).css("color", randomColor);
    }
 }
 
-$("a").hover(function(){
+$(".imageAction").mouseenter(function(){
    var randomColor = palette[Math.floor(Math.random() * palette.length)];
-   tempColor = $(this).css("color");
-   $(this).css("color", randomColor);
-   }, function(){
-   $(this).css("color", tempColor);
-   tempColor = "#F5F5F5";
+   var css = ".imageAction:hover { color:" + randomColor + " }";
+   var style = document.createElement("style");
+
+   if (style.styleSheet) {
+       style.styleSheet.cssText = css;
+   } else {
+       style.appendChild(document.createTextNode(css));
+   }
+
+   document.getElementsByTagName("head")[0].appendChild(style);
+});
+
+$(".MxR").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".MxR").css("height", "30vw");
+});
+
+$(".ARoS").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".ARoS").css("height", "30vw");
+});
+
+$(".STEAM").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".STEAM").css("height", "30vw");
+});
+
+$(".XR").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".XR").css("height", "30vw");
+});
+
+$(".Printing").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".Printing").css("height", "30vw");
+});
+
+$(".CompVis").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".CompVis").css("height", "30vw");
+});
+
+$(".Education").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".Education").css("height", "30vw");
+});
+
+$(".Maker").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".Maker").css("height", "30vw");
+});
+
+$(".OpenSource").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".OpenSource").css("height", "30vw");
+});
+
+$(".Robotics").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".Robotics").css("height", "30vw");
+});
+
+$(".Light").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".Light").css("height", "30vw");
+});
+
+$(".Climbing").mouseenter(function(){
+   $(".image").css("height", "0px");
+   $(".Climbing").css("height", "30vw");
 });
 
 dynamicSkills();
