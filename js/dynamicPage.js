c85d9ec2b07a5e42c298b8872df858d667c7a989
cbork <cbork13@gmail.com>
2021-08-02 12:43:53 -0400

    added headset thumbnail, updated image transitions and layout.

--- js/dynamicPage.js
+++ js/dynamicPage.js
@@ -21,7 +21,6 @@ function dynamicContact() {
    $("#dynamicContact").css("color", randomColor);
    iC = (iC+1) % iContact.length;
 }
-dynamicContact();
 
 function dynamicCopywrite() {
    var currentYear = date.getFullYear();
@@ -51,66 +50,91 @@ $(".imageAction").mouseenter(function(){
 });
 
 $(".MxRHead").hover(function(){
-      $(".image").css("height", "0px");
-      $(".MxR").css("height", "30vw");
+   $(".image").css("height", "0px");
+   $(".image").css("width", "0px");
+   $(".MxR").css("height", "15vw");
+   $(".MxR").css("width", "15vw");
 });
 
 $(".ARoSHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".ARoS").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".ARoS").css("height", "15vw");
+   $(".ARoS").css("width", "15vw");
 });
 
 $(".STEAMHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".STEAM").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".STEAM").css("height", "15vw");
+   $(".STEAM").css("width", "15vw");
 });
 
 $(".XRHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".XR").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".XR").css("height", "15vw");
+   $(".XR").css("width", "15vw");
 });
 
 $(".PrintingHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".Printing").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".Printing").css("height", "15vw");
+   $(".Printing").css("width", "15vw");
 });
 
 $(".CompVisHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".CompVis").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".CompVis").css("height", "15vw");
+   $(".CompVis").css("width", "15vw");
 });
 
 $(".EducationHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".Education").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".Education").css("height", "15vw");
+   $(".Education").css("width", "15vw");
 });
 
 $(".MakerHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".Maker").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".Maker").css("height", "15vw");
+   $(".Maker").css("width", "15vw");
 });
 
 $(".OpenSourceHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".OpenSource").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".OpenSource").css("height", "15vw");
+   $(".OpenSource").css("width", "15vw");
 });
 
 $(".RoboticsHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".Robotics").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".Robotics").css("height", "15vw");
+   $(".Robotics").css("width", "15vw");
 });
 
 $(".LightHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".Light").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".Light").css("height", "15vw");
+   $(".Light").css("width", "15vw");
 });
 
 $(".ClimbingHead").mouseenter(function(){
    $(".image").css("height", "0px");
-   $(".Climbing").css("height", "30vw");
+   $(".image").css("width", "0px");
+   $(".Climbing").css("height", "15vw");
+   $(".Climbing").css("width", "15vw");
 });
 
 dynamicSkills();
+dynamicContact();
 dynamicCopywrite();
 randomHighlight();
 setInterval(dynamicSkills, 1319);
